6e84a32c130be474cdbae6d18c74a77aeb41ea5e
cleitonreis-dev <klleiton@gmail.com>
2019-02-07 00:19:19 +0100

    generic stack

---
+++ src/main/java/com/xplusj/Stack.java
@@ -0,0 +1,31 @@
+package com.xplusj;
+
+import lombok.AllArgsConstructor;
+
+public class Stack<T> {
+
+    private Node<T> head;
+
+    public void push(T value){
+        head = new Node<>(value, head);
+    }
+
+    public T pull(){
+        if(head == null)
+            throw new IllegalStateException("Empty stack");
+
+        T value = head.value;
+        head = head.next;
+        return value;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    @AllArgsConstructor
+    private static class Node<T>{
+        private final T value;
+        private final Node<T> next;
+    }
+}
